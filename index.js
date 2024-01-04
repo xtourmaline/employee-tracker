cbfeb58946ae5faf2e130f046171ff52ed5388b4
Vanessa Trinh <xtourmaline@gmail.com>
2024-01-04 00:28:27 -0500

    created connection to database

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const { table } = require('table');
-
+const { table } = require("table");
 const mysql = require('mysql2/promise');
+const { default: inquirer } = require("inquirer");
 
+const init = async () => {
+    const db = await mysql.createConnection(
+        {
+            host:"localhost",
+            user: "root",
+            password: "blank123",
+            database: "tracker_db"
+        }
+    );
+    console.log("Connected to the tracker_db database.");
+    
+    const options = {
+        name: "task",
+        type: "",
+
+    }
+
+    const results = await db.query("SELECT * FROM DEPARTMENT");
+    console.log(results);
+};
+
+init();
